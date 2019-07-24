a967cf951eedab7400009d3ea1374449f73cf011
Eade_Jiang <1085109351@qq.com>
2019-07-24 21:27:54 +0800

    项目大体搭建

---
+++ biyao/src/pages/Classify/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Classify = ()=><div>分类</div>
+
+export default Classify;
---
+++ biyao/src/pages/Home/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = ()=><div>首页</div>
+
+export default Home;
