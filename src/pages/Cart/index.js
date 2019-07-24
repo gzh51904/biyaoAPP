eadcb82fa5f7d7526f8fa700912269cbab3a6972
Eade_Jiang <1085109351@qq.com>
2019-07-24 22:30:26 +0800

    修改部分细节

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -16,80 +16,80 @@ class Cart extends Component{
         let {datalist} = this.state;
         return (
             //购物车不为空时
-            // <div class="container by_w" id="by_w">
-            //     <div class="main-title">
-            //         <div class="title-site"></div>
-            //         <div class="title-fix">
+            // <div className="container by_w" id="by_w">
+            //     <div className="main-title">
+            //         <div className="title-site"></div>
+            //         <div className="title-fix">
             //             <a href="/"><Icon className="icon" type="home" style={{ fontSize: '16px', color: '#333' }}/></a>
-            //             <p class="escp">购物车</p>
-            //             <span id="editAll" class="">编辑</span>
+            //             <p className="escp">购物车</p>
+            //             <span id="editAll" className="">编辑</span>
             //         </div>
             //     </div>
-            //     <div id="panel-set" class="">
-            //         <div class="shop-panel">
-            //             <div class="shop-panel-store border-b-1 clear">
-            //                 <div class="float-right"><span class="store-edit" data-edit="1">编辑</span></div>
-            //                 <div class="float-left">
-            //                     <b class="store-check">
-            //                         <span class="nocheck none">
+            //     <div id="panel-set" className="">
+            //         <div className="shop-panel">
+            //             <div className="shop-panel-store border-b-1 clear">
+            //                 <div className="float-right"><span className="store-edit" data-edit="1">编辑</span></div>
+            //                 <div className="float-left">
+            //                     <b className="store-check">
+            //                         <span className="nocheck none">
             //                             <Icon className="icon" type="home" style={{ fontSize: '16px', color: '#333' }}/>
             //                         </span>
-            //                         <span class="checked">
+            //                         <span className="checked">
             //                             <Icon className="icon" type="home" style={{ fontSize: '16px', color: '#333' }}/>
             //                         </span>
             //                     </b>
-            //                     <img src="https://static.biyao.com/mnew/img/master/shopCar/store-logo_87d05ed.png" class="store-logo" />
-            //                     <a class="store-name" href="/classify/supplierHome?supplierId=130300">FRC</a>
-            //                     <span class="icon-arrow-right">
+            //                     <img src="https://static.biyao.com/mnew/img/master/shopCar/store-logo_87d05ed.png" className="store-logo" />
+            //                     <a className="store-name" href="/classify/supplierHome?supplierId=130300">FRC</a>
+            //                     <span className="icon-arrow-right">
             //                         <Icon className="icon" type="home" style={{ fontSize: '16px', color: '#333' }}/>
             //                     </span>
             //                 </div>
             //             </div>
-            //             <div class="shop-panel-product-set">
-            //                 <div class="shop-panel-product-item border-b-1 clear">
-            //                     <div class="float-left icon-radio">
-            //                         <b class="su-check">
-            //                             <span class="nocheck none" data-shopcarid="813235277" data-specialproducttype="0">
+            //             <div className="shop-panel-product-set">
+            //                 <div className="shop-panel-product-item border-b-1 clear">
+            //                     <div className="float-left icon-radio">
+            //                         <b className="su-check">
+            //                             <span className="nocheck none" data-shopcarid="813235277" data-specialproducttype="0">
             //                                 <Icon className="icon" type="home" style={{ fontSize: '16px', color: '#333' }}/>
             //                             </span>
-            //                             <span class="checked" data-shopcarid="813235277" data-specialproducttype="0">
+            //                             <span className="checked" data-shopcarid="813235277" data-specialproducttype="0">
             //                                 <Icon className="icon" type="home" style={{ fontSize: '16px', color: '#333' }}/>
             //                             </span>
             //                         </b>
             //                     </div>
-            //                     <div class="product-pic border-1">
-            //                         <a href="javascript:;" class="aLinkJump" data-href="/products/1303005003010000001.html">
-            //                             <img class="lazy" src="https://bfs.biyao.com/group1/M00/F6/98/rBACVF0IN5aALph_AACJL5soNkY303_400x400.jpg" />
+            //                     <div className="product-pic border-1">
+            //                         <a href="javascript:;" className="aLinkJump" data-href="/products/1303005003010000001.html">
+            //                             <img className="lazy" src="https://bfs.biyao.com/group1/M00/F6/98/rBACVF0IN5aALph_AACJL5soNkY303_400x400.jpg" />
             //                         </a>
             //                     </div>
-            //                     <div class="product-detail">
-            //                         <div class="product-name"><a href="/products/1303005003010000001.html">魅力缪斯清透散粉</a></div>
-            //                         <div class="product-info">
-            //                             <div class="product-dec">粉色</div>
-            //                             <div class="product-buy">
-            //                                 <span class="product-price">
+            //                     <div className="product-detail">
+            //                         <div className="product-name"><a href="/products/1303005003010000001.html">魅力缪斯清透散粉</a></div>
+            //                         <div className="product-info">
+            //                             <div className="product-dec">粉色</div>
+            //                             <div className="product-buy">
+            //                                 <span className="product-price">
             //                                 ￥<i data-surfingprice="8900">89</i>
-            //                                 </span>×<span class="product-num">1</span>
+            //                                 </span>×<span className="product-num">1</span>
             //                             </div>
             //                         </div>
-            //                         <div class="product-edit clear none" data-shopcarid="813235277" data-suid="1303005003010000001">
-            //                             <span class="del-btn float-right">
+            //                         <div className="product-edit clear none" data-shopcarid="813235277" data-suid="1303005003010000001">
+            //                             <span className="del-btn float-right">
             //                                 <Icon className="icon" type="home" style={{ fontSize: '16px', color: '#333' }}/>
             //                             </span>
-            //                             <div class="maparea">
-            //                                 <span class="reduce-btn font-disable">-</span>
-            //                                 <span class="su-num">1</span>
-            //                                 <span class="add-btn ">+</span>
+            //                             <div className="maparea">
+            //                                 <span className="reduce-btn font-disable">-</span>
+            //                                 <span className="su-num">1</span>
+            //                                 <span className="add-btn ">+</span>
             //                             </div>
             //                         </div>
             //                     </div>
-            //                     <div class="product-else"></div>
+            //                     <div className="product-else"></div>
             //                 </div>
             //             </div>
             //         </div>
-            //         <div class="shopcar-empty none" id="shopcar-empty">
+            //         <div className="shopcar-empty none" id="shopcar-empty">
             //         <img src="https://static.biyao.com/mnew/img/null/shopcar_6b91960.png" width="50px" />
-            //         <div class="empty-til">购物车还是空的，赶紧行动吧！</div>
+            //         <div className="empty-til">购物车还是空的，赶紧行动吧！</div>
             //         <a href="/">去逛逛</a>
             //     </div>
             //     </div>
@@ -97,19 +97,19 @@ class Cart extends Component{
 
 
             //购物车为空时显示
-            <div class="container by_w" id="by_w">
-	            <div class="main-title">
-		            <div class="title-site"></div>
-		            <div class="title-fix">
+            <div className="container by_w" id="by_w">
+	            <div className="main-title">
+		            <div className="title-site"></div>
+		            <div className="title-fix">
 			            <a href="/"><Icon className="icon" type="home" style={{ fontSize: '16px', color: '#333' }}/></a>
-			            <p class="escp">购物车</p>
-			            <span id="editAll" class="none">编辑</span>
+			            <p className="escp">购物车</p>
+			            <span id="editAll" className="none">编辑</span>
 		            </div>
 	             </div>
-	            <div id="panel-set" class="none"></div>
-	            <div class="shopcar-empty" id="shopcar-empty">
+	            <div id="panel-set" className="none"></div>
+	            <div className="shopcar-empty" id="shopcar-empty">
 		            <img src="https://static.biyao.com/mnew/img/null/shopcar_6b91960.png" width="50px" />
-		            <div class="empty-til">购物车还是空的，赶紧行动吧！</div>
+		            <div className="empty-til">购物车还是空的，赶紧行动吧！</div>
 		            <a href="/">去逛逛</a>
 	            </div>
             </div> 
